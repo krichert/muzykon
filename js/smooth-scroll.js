854efb35f62005021645d460dc5341035e13933d
Kamil Richert <kamil.richert92@gmail.com>
2019-09-05 00:06:36 +0200

    Added heading, place for hero and functions section

---
+++ js/smooth-scroll.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  const $navMenuItems = $('.nav-menu__list  a');
+
+  $('a[href^="#"]').on('click', function (event) {
+
+    var target = $($(this).attr('href'));
+
+    $navMenuItems.each(function () {
+      $(this).removeClass('nav-menu__item--active');
+    });
+
+    $(this).addClass('nav-menu__item--active');
+
+    if (target.length) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top - 56
+      }, 1000);
+    }
+  });
+});
