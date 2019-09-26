9609c56ad0d929a13c17626ecfd9eaeaccc9fb6a
Kamil Richert <kamil.richert92@gmail.com>
2019-09-26 13:34:08 +0200

    Update firebase.config.js

--- js/firebase.config.js
+++ js/firebase.config.js
@@ -1,12 +1,12 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyDJuH2YQ6V9obmJYb4Bpz_my2omkrBI1Yw",
-    authDomain: "muzykon-e45fd.firebaseapp.com",
-    databaseURL: "https://muzykon-e45fd.firebaseio.com",
-    projectId: "muzykon-e45fd",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "",
-    messagingSenderId: "677024028662",
-    appId: "1:677024028662:web:265788ae3e85edc66599d7"
+    messagingSenderId: "",
+    appId: ""
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
